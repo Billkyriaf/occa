75379497346c6d12fc447e88c809a4ce3fe9ded9
Vasilis Kyriafinis <bkyriafinis@gmail.com>
2026-07-30 04:59:20 +0300

    Adds XRT libraries import utilities

---
+++ src/occa/internal/modes/xrt.hpp
@@ -0,0 +1,11 @@
+#include <occa/defines.hpp>
+
+#if OCCA_XRT_ENABLED
+
+#ifndef OCCA_INTERNAL_MODES_XRT_HEADER
+#define OCCA_INTERNAL_MODES_XRT_HEADER
+
+#include <occa/internal/modes/xrt/polyfill.hpp>
+
+#endif
+#endif
---
+++ src/occa/internal/modes/xrt/polyfill.hpp
@@ -0,0 +1,16 @@
+#include <occa/defines.hpp>
+
+#ifndef OCCA_INTERNAL_MODES_XRT_POLYFILL_HEADER
+#define OCCA_INTERNAL_MODES_XRT_POLYFILL_HEADER
+
+#if OCCA_XRT_ENABLED
+
+#include <xrt.h>
+#include <xrt/xrt_bo.h>
+#include <xrt/xrt_device.h>
+#include <xrt/xrt_kernel.h>
+#include <xrt/xrt_uuid.h>
+
+#endif
+
+#endif
